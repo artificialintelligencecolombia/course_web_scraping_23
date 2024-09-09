21d6ef9cfea23fb34007344906bb9b3ab33f47ac
artificialintelligencecolombia <damarias991@gmail.com>
2024-09-08 22:13:38 -0500

    san vicente url - property description

--- ch3_strating_crawl/finca_raiz_crawler/second_scraper.py
+++ ch3_strating_crawl/finca_raiz_crawler/second_scraper.py
@@ -7,22 +7,38 @@ def get_info(page_url):
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36'}
     req = Request(url, headers=headers)
 
+    # Fetching and parsing html with bs4
     html = urlopen(req)
-    bsObj = BeautifulSoup(html, "html.parser")
+    bsObj = BeautifulSoup(html, "html.parser", from_encoding="utf-8")
 
-    # Find all relevant span tags
+    # Find all span tags regarding to property_price
     price_spans = bsObj.findAll("span", {"class": "ant-typography price heading heading-3 high"})
     # print(price_spans)
 
-    prices = []
+    # Find all span tags regarding to property description
+    description_spans = bsObj.findAll("span", {"class": "body body-2 body-regular medium"})
+
+    # print(description_spans)
+
+    # Extract property descr. details
+    strong_list = []
+    for span in description_spans:
+        strong_tags = span.findAll("strong")
+        for strong in strong_tags:
+            strong_list.append(strong.get_text())
+
+    # print(strong_list)
+
+    # Extract property prices
+    property_price = []
     for span in price_spans:
         strong_tag = span.find("strong")
         # print(strong_tag)
 
         if strong_tag:
-            prices.append(strong_tag.get_text(strip=True))
+            property_price.append(strong_tag.get_text(strip=True))
     
-    return prices
+    return property_price
 
 list = get_info("")
-print(list)
+# print(list)
