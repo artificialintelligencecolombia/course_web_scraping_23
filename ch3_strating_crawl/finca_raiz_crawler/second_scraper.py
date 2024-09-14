a0ba995f44ec63cc6980d242d5ed81cdb7465e0b
Daniel Maldonado <damarias991@gmail.com>
2024-09-13 21:46:46 -0500

    Styling file

--- ch3_strating_crawl/finca_raiz_crawler/second_scraper.py
+++ ch3_strating_crawl/finca_raiz_crawler/second_scraper.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 import pandas as pd
 
 def get_info(page_url):
+    # -------------- MAKING REQUEST -> CREATE THE BSOBJECT --------------------------------
     url = "https://www.fincaraiz.com.co/venta/fincas/san-vicente/antioquia" + page_url
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36'}
     req = Request(url, headers=headers)
@@ -12,6 +13,7 @@ def get_info(page_url):
     html = urlopen(req)
     bsObj = BeautifulSoup(html, "html.parser", from_encoding="utf-8")
 
+    # --------------------FINDING THE LIST OF ELEMENTS
     # Find all span tags regarding to property_price
     price_spans = bsObj.findAll("span", {"class": "ant-typography price heading heading-3 high"})
     #print(price_spans)
@@ -23,7 +25,7 @@ def get_info(page_url):
     # Fetch all strong tags related to the realtors
     realtor_strongs = bsObj.findAll("strong", {"class": "body body-2 high"})
     
-
+    # ------------------ PYTHON LISTST -----------------------
     # Extract property descr. details
     data = []
     for span in description_spans:
@@ -33,19 +35,12 @@ def get_info(page_url):
 
     # Original list of data where the elements are mixed in sequence of 3 for each r.e.)
     # print(data)
-
+    
+    result = [data[i:i + 3] for i in range(0, len(data), 3)]
     # Use list comprehension to group every 3 consecutive elements into a sublist
     # Breakdown:
     # - data[i:i+3]: Slices the list into sublists of 3 elements (start at index i, end at i+3)
     # - range(0, len(data), 3): Iterates over the list with steps of 3 (i = 0, 3, 6, ...)
-    result = [data[i:i + 3] for i in range(0, len(data), 3)]
-
-    df = pd.DataFrame(result,columns=['Bedrooms', 'Bathromms','Area'])
-    # Print the resulting list of sublists
-    #print(df)
-
-    # Create a DataFrame with columns for Bedrooms, Bathrooms, and Size
-    # print(strong_list)
 
     # Extract property prices
     property_price = []
@@ -59,12 +54,18 @@ def get_info(page_url):
     realtor = []
     for strong in realtor_strongs:
         realtor.append(strong.get_text())
-
-    # Insert property_price and realtor columns into df
     
+    # -------------- CREATION OF DATAFRAME, APPENDING COLUMNS
+    df = pd.DataFrame(result,columns=['Bedrooms', 'Bathromms','Area'])
+    # Create a DataFrame with columns for Bedrooms, Bathrooms, and Size
+    # print(strong_list)
+    
+    # Print the resulting list of sublists
+    #print(df)
+
     df.insert(0, 'Price', property_price)
     df.insert(4, 'Realtor', realtor)
-
+    # Insert property_price and realtor columns into df
 
     # Set display option to show all columns
     pd.set_option('display.max_columns', None)
